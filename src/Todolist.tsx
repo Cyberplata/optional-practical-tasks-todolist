72a463cd62313e13d5de5e24ec3439383d17811a
PlatonKynin <kininplaton@gmail.com>
2024-04-29 23:26:05 +0300

    01sprint-3week-3optionalTask->Сделал 1 и 2 пункт задачи

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -13,21 +13,26 @@ type PropsType = {
     removeTask: (taskId: string) => void
     changeFilter: (value: FilterValuesType) => void
     addTask: (title: string) => void
+    children?: React.ReactNode
 }
 
-export function Todolist(props: PropsType) {
+export const Todolist: React.FC<PropsType> = ({children, ...props}) => {
     let [title, setTitle] = useState("")
+    let onChangeRef = useRef<HTMLInputElement>(null)
 
     const addTask = () => {
-        props.addTask(title);
-        setTitle("");
-    }
+        // props.addTask(title);
+        // setTitle("");
+        if (onChangeRef.current) {
+            props.addTask(onChangeRef.current.value)
+            setTitle("")
+            // onChangeRef.current.value = ''
+        }
 
+    }
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
     }
-
-
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
             addTask();
@@ -41,9 +46,15 @@ export function Todolist(props: PropsType) {
     return <div>
         <h3>{props.title}</h3>
         <div>
-            <input value={title}
-                   onChange={onChangeHandler}
-                   onKeyPress={onKeyPressHandler}
+            {/*<input value={title}*/}
+            {/*       onChange={onChangeHandler}*/}
+            {/*       onKeyPress={onKeyPressHandler}*/}
+            {/*/>*/}
+            <input
+                value={title}
+                onChange={onChangeHandler}
+                ref={onChangeRef}
+                onKeyPress={onKeyPressHandler}
             />
             <button onClick={addTask}>+</button>
         </div>
@@ -66,23 +77,9 @@ export function Todolist(props: PropsType) {
             <button onClick={onActiveClickHandler}>Active</button>
             <button onClick={onCompletedClickHandler}>Completed</button>
         </div>
+        {children}
     </div>
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //----------------------------------------------------------------------------------
